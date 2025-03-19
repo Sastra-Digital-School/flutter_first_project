a035b738814cde5240ff795d37c649059a46a780
Sithy <sithy@Sithys-MacBook-Pro.local>
2025-03-19 19:45:35 +0700

    Your commit message here

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,9 +6,25 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          children: const [Text('Welcome to the Home Screen')],
+          children: [
+            const Text(
+              'Welcome to the Home Screen',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter Your Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: const Text('Submit')),
+          ],
         ),
       ),
     );
