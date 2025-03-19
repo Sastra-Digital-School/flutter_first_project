354a635f621410225ae10dbbc3bc1a819d70cc4f
Chamroeun Sithy <90774658+ChamroeunSithy@users.noreply.github.com>
2025-03-19 19:34:41 +0700

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -10,7 +10,6 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('This is veasna development'),
             const SizedBox(height: 50),
